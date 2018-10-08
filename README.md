d13a666a427451ec16678010992a3e0836e7a9ed
Damian Baar <damian.baar@gmail.com>
2018-10-08 15:22:40 +0200

    Moving cli-preset to opensource

---
+++ README.md
@@ -0,0 +1,15 @@
+# @hungry/babel-preset-cli
+
+Babel preset for Node.js, with typescript and import/export syntax.
+
+## Install
+```sh
+yarn add @hungry/babel-preset-cli
+```
+
+## Node API
+```js
+// within node.js script
+const { bootstrap } = require('@hungry/babel-preset-cli')
+bootstrap()
+```
---
+++ index.js
@@ -0,0 +1,35 @@
+const { declare } = require("@babel/helper-plugin-utils")
+
+const preset = declare((api, opts) => ({
+  presets: [
+    [
+      "@babel/env",
+      {
+        targets: {
+          node: "current"
+        }
+      }
+    ],
+    "@babel/preset-typescript",
+  ],
+  plugins: [
+    "@babel/plugin-proposal-export-default-from",
+    "@babel/plugin-proposal-class-properties",
+    "@babel/plugin-proposal-object-rest-spread",
+    "babel-plugin-dynamic-import-node",
+    "@babel/transform-react-jsx",
+    "@babel/plugin-syntax-dynamic-import",
+  ]
+}))
+
+preset.bootstrap = ({ cwd, presets } = {}) =>
+  require("@babel/register")({
+    ignore: [/node_modules/],
+    presets: ['@hungry/babel-preset-cli'].concat([] || presets),
+    extensions: [".ts", ".tsx"],
+    cwd,
+    babelrc: true,
+    cache: true
+  })
+
+module.exports = preset
